4d3ba2a139ab96c6a0c64805622ac63a14420823
Vasily Valov <vasilyvalov@gmail.com>
2015-08-30 01:59:52 +0300

    Arguments divided on mandatory and optional.

--- smtptool.py
+++ smtptool.py
@@ -5,31 +5,33 @@ import argparse
 #
 # dictionary with SMTP session settings
 #
-smtp_options = {"to":'test@localhost',"from":'test@localhost',
-                "server":'localhost',"body":'',
-                "ehlo":'localhost',"attach":''}
+smtp_options = {"--to":['test@localhost', "recipient of the message"],
+                "--from":['test@localhost',"sender of the message."],
+                "--server":['localhost',"IP address or hostname of a server to connect"],
+                "body":['',"You can use prepared eml file to fill message body."],
+                "--ehlo":['localhost',"EHLO value in SMTP session"],
+                "--attach":['',"Attach file to a message"]}
 
 # additional arguments about number of messages/sessions and etc
 """ number of concurrent threads. Number of messages that will be send
     is calculated as multiplication of threads and repeat values.
     N = Threads * Repeat  
 """
-extra_options = { "dir":'', # directory contains prepared eml files to send
-                  "repeat":1, # send email N times. By default send one email only once.
-                  "threads":1,
+extra_options = { "--dir":['',"Use dir option to send the whole content of directory"], # directory contains prepared eml files to send
+                  "--repeat":[1,"Send email N times. Default value is 1"], # send email N times. By default send one email only once.
+                  "--threads":[1,"Number of concurrent threads sending emails. Default : 1 thread."]
 }
 
                                                   
 def args_initialization():
     parser = argparse.ArgumentParser()
     for k,v in smtp_options.items():
-        parser.add_argument(k)
-        print("Adding new argument:  " + k)
-
+        parser.add_argument(k,help=v[1])
+       
     for  k,v in extra_options.items():
-        parser.add_argument(k)
+        parser.add_argument(k,help=v[1])
 
-    parser.parse_args()
+    args = parser.parse_args()
 
 
 args_initialization()
